bc4dbc3d1ba8272ff5e86bc174efae482e3fea45
SunGuk <sgk0713@estsoft.com>
2023-12-17 23:41:30 +0900

    Fix api response parameter as nullable for when a field is missing

--- data/src/main/java/com/sunguk/data/dto/SearchBookResponse.kt
+++ data/src/main/java/com/sunguk/data/dto/SearchBookResponse.kt
@@ -5,8 +5,8 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class SearchBookResponse(
-    @Json(name = "error") val error: String,          // "0"
-    @Json(name = "total") val total: String,          // "333"
-    @Json(name = "page") val page: String,           // "1"
+    @Json(name = "error") val error: String?,          // "0"
+    @Json(name = "total") val total: String?,          // "333"
+    @Json(name = "page") val page: String?,           // "1"
     @Json(name = "books") val books: List<BookData>,
 )
