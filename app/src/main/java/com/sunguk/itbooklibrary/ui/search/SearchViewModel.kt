6035d4353e4bdee68e0f0de87c90225bab7c6977
SunGuk <sgk0713@estsoft.com>
2023-12-17 16:31:35 +0900

    Implement SearchResultController interface

--- app/src/main/java/com/sunguk/itbooklibrary/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/sunguk/itbooklibrary/ui/search/SearchViewModel.kt
@@ -1,13 +1,41 @@
 package com.sunguk.itbooklibrary.ui.search
 
 import com.sunguk.itbooklibrary.ui.base.BaseViewModel
+import com.sunguk.itbooklibrary.ui.search.adapter.SearchResultController
 import com.sunguk.itbooklibrary.ui.search.intent.SearchEvent
 import com.sunguk.itbooklibrary.ui.search.intent.SearchState
+import com.sunguk.itbooklibrary.ui.search.item.SearchResultBook
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-) : BaseViewModel<SearchState, SearchEvent>(SearchState()) {
+) : BaseViewModel<SearchState, SearchEvent>(SearchState()), SearchResultController {
 
+    private val loadMoreItem = SearchResultBook.LoadMore()
+
+
+    override fun onLoadMoreClicked() {
+        launch {
+            state.copy(
+                searchedBook = state.searchedBook - loadMoreItem
+            ).let {
+                updateState(it)
+            }
+        }
+        // TODO: call search again
+    }
+
+    override fun onOpenLinkClicked(url: String) {
+        launch {
+            sendEvent(SearchEvent.OpenWebLink(url))
+        }
+    }
+
+    override fun onItemClicked(isbn13: String) {
+        launch {
+            sendEvent(SearchEvent.NavToDetail(isbn13))
+        }
+    }
 }
